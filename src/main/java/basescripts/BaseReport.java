8fb90d0e77bfb38914f837e951bc75bc85e47f90
team-selenium-neox <manfred.hornberger@neox.de>
2020-05-08 11:17:38 +0200

    first commit

--- src/main/java/basescripts/BaseReport.java
+++ src/main/java/basescripts/BaseReport.java
@@ -192,7 +192,157 @@ public class BaseReport {
 		fw.write("</head>\n");
 		fw.write("<body>\n");
 		fw.write("<form name=\"myForm>\" \n");
-		fw.write("<div style = \"position:absolute; left:5px; pedding:5px; spacing:5px; boarder: 1px solid #08298A; width: 1115px; height: 225px; background-color: #e8e8e8\"> \n");
-//kack leerstell
+		fw.write("<div style = \"position:absolute; left:5px; padding:5px; spacing:5px; boarder: 1px solid #08298A; width: 1115px; height: 225px; background-color: #e8e8e8\"> \n");
+		fw.write("<table width=\"1102\"> \n");
+		fw.write("<tr> \n");
+		fw.write("<td colspan=\"2\" class=\"aemCellGrey\" style=\"color:#0829A8; font-size:20px;\">SBT-Report</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> Short Description:&#160; </td><td class=\"aemCellGreyNormal\">SBT-Report-Regression-Test \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> Execution Date:&#160; </td><td class=\"aemCellGreyNormal\"> \n");
+		fw.write(currentDate);
+		fw.write("</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> Test Object:&#160; </td><td class=\"aemCellGreyNormal\">"+proper.getPropValue(configFile, "projectname")+"</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> OS System:&#160; </td><td class=\"aemCellGreyNormal\"> \n");
+		fw.write(System.getProperty("os.name"));
+		fw.write("</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> JDK:&#160; </td><td class=\"aemCellGreyNormal\"> \n");
+		fw.write(System.getProperty("java.runtime.version"));
+		fw.write("</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> Browser:&#160; </td><td class=\"aemCellGreyNormal\"><input type=\"text\" id=\"browserField\" class=\"displayFieldInfo\" value=''\" \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"> Test Environment:&#160; </td><td class=\"aemCellGreyNormal\">"+proper.getPropValue(configFile, "targetEnv")
+		+ "<input type='text' id='spacer' class='displayFieldInfo' value='' readonly style='width:252px;'/>"
+		+ "<input type='text' id='startTimeLabel' class='displayFieldInfo' value='Start-Time' readonly style='width:70px;'/>"
+		+ "<input type='text' id='startTime' class='displayFieldInfo' value='' readonly style='width:98px;'/>"
+		+ "<input type='text' id='endTimeLabel' class='displayFieldInfo' value='End-Time' readonly style='width:65px;'/>"
+		+ "<input type='text' id='endTime' class='displayFieldInfo' value='' readonly />"
+		+ "</td>\n");
+		fw.write("</tr> \n");
+		fw.write("</table> \n");
+		
+		fw.write("<table width=\"1102\">\n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\"width=\"600\" style=\"color:#08298A; font-size:13px;\">&#160;</td> \n");
+		fw.write("<td class=\"aemCellGrey\"width=\"0\" style=\"color:#08298A; font-size:13px;\">Checks: <input type=\"text\" id=\"cField\" class=\"displayField\" value='';</td> \n");
+		fw.write("<td class=\"aemCellGrey\"width=\"0\" style=\"color:#08298A; font-size:13px; color: green\">Passed: <input type)\"text\" id=\"pField\" class=\"displayField\" value='';</td> \n");
+		fw.write("<td class=\"aemCellGrey\"width=\"0\" style=\"color:#08298A; font-size:13px; color: red\">Failed: <input type)\"text\" id=\"fField\" class=\"displayField\" value='';</td> \n");
+		fw.write("</tr> \n");
+		fw.write("</table> \n");
+		
+		fw.write("</div>\n");
+		fw.write("<br/>\n");
+		fw.write("<div style = \"position:absolute; left:-1px; padding:5px; spacing:5px; boarder: 1px solid #08298A; width: 1115px; height: 225px; background-color: #e8e8e8\"> \n");
+		fw.close();
+		
+		startTime = getDate("SDF1").substring(11);
+		
 	}
+	
+	/** 
+	 * 
+	 * generate the Report
+	 * 
+	 * */
+	public static void createaemReportAdding(String methodname, String checkQuestion, String expectedStr, String foundStr, String errMsg) throws IOException {
+		
+		String mPassed = "PASSED";
+		String mFailed = "FAILED";
+		
+		checkedCounter = checkedCounter +1;
+		
+		String fileName = "";
+		
+		fileName = aemReportPath +"screenshots" + File.separator + reportName;
+		
+		FileWriter fw = new FileWriter(fileName, true);
+		
+		fw.write("<table border=\"1\" class=\"fixed\">\n");
+		fw.write("<col width=\"299px\"/>\n");
+		fw.write("<col width=\"299px\"/>\n");
+		fw.write("<col width=\"54px\"/>\n");
+		fw.write("<col width=\"80px\"/>\n");
+		fw.write("<col width=\"370px\"/>\n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellBlue\" colspan=\"5\">");
+		fw.write(methodname);
+		fw.write("</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellYellow\" colspan=\"5\">Check:");
+		fw.write("&#160;");
+		fw.write(checkQuestion);
+		fw.write("</td> \n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellGrey\">Target State</td>\n");
+		fw.write("<td class=\"aemCellGrey\">Current State</td>\n");
+		fw.write("<td class=\"aemCellGrey\" style=\"text-align:center;\"> State</td>\n");
+		fw.write("<td class=\"aemCellGrey\" style=\"text-align:center;\"> Screenshot</td>\n");
+		fw.write("<td class=\"aemCellGrey\"> Error Msg</td>\n");
+		fw.write("</tr> \n");
+		fw.write("<tr> \n");
+		fw.write("<td class=\"aemCellWhiteNormal\">");
+		fw.write(expectedStr);
+		fw.write("</td> \n");
+		fw.write("<td class=\"aemCellWhiteNormal\">");
+		fw.write(foundStr);
+		fw.write("</td> \n");
+		
+		if(!foundStr.isEmpty()) {
+			if (expectedStr.equals(foundStr)) {
+				softAssertion.assertEquals(foundStr, expectedStr);
+				// ====PASSED====
+				passedCounter = passedCounter +1;
+				fw.write("<td class=\"aemCellWhiteNormal\" style=\"text-align:center;\">");
+				fw.write(mPassed);
+				fw.write("</td> \n");
+				fw.write("<td class=\"aemCellWhiteNormal\" style=\"text-align:center;\"> </td>\n");
+				fw.write("<td class=\"aemCellWhiteNormal\">");
+				fw.write(errMsg);
+				fw.write("</td> \n");
+			}else {
+				softAssertion.assertEquals(foundStr, expectedStr);
+				// ====FAIL====
+				failedCounter = failedCounter +1;
+				takeScreenshot("string_not_matched");
+				fw.write("<td class=\"aemCellRedNormal\" style=\"text-align:center;\">");
+				fw.write(mFailed);
+				fw.write("</td> \n");
+				fw.write("<td class=\"aemCellRedNormal\" style=\"text-align:center;\"><A TARGET =\"_blank\" HREF=\"");
+				fw.write(screenshotFileLink);
+				fw.write("\">Screenshot</A></td>\n");
+				fw.write("<td class=\"aemCellRedNormal\">");
+				fw.write(errMsg1);
+				fw.write("</td> \n");
+			}
+		}else {
+			failedCounter = failedCounter +1;
+			fw.write("<td class=\"aemCellRedNormal\" style=\"text-align:center;\">");
+			fw.write(mFailed);
+			fw.write("<td class=\"aemCellRedNormal\" style=\"text-align:center;\"><A TARGET =\"_blank\" HREF=\"");
+			fw.write(screenshotFileLink);
+			fw.write("\">Screenshot</A></td>\n");
+			fw.write("<td class=\"aemCellRedNormal\">");
+			fw.write(errMsg1);
+			fw.write("</td> \n");
+		}
+		fw.write("</tr> \n");
+		fw.write("</table>\n");
+		fw.write("\n");
+		
+		fw.close();
+	}
+	
 }
